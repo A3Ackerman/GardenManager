2b476186715c7eb06e536cc1d66c74737087c12c
jgidds <50605198+jgidds@users.noreply.github.com>
2020-10-22 18:22:37 -0600

    Fix trailing spaces in populate scripts

--- Populate_plantfamily.sql
+++ Populate_plantfamily.sql
@@ -1,20 +1,20 @@
 INSERT INTO "GardenManager".plantfamily (genus,plantfamily) VALUES
-	 ('Allium                        ','Alliaceae                     '),
-	 ('Beta                          ','Chenopodiaceae                '),
-	 ('Brassica                      ','Brassicaceae                  '),
-	 ('Capsicum                      ','Solanaceae                    '),
-	 ('Convallaria                   ','Convallariaceae               '),
-	 ('Cucumis                       ','Cucurbitaceae                 '),
-	 ('Daucus                        ','Apiaceae                      '),
-	 ('Fragaria                      ','Rosaceae                      '),
-	 ('Lactuca                       ','Asteraceae                    '),
-	 ('Lavendula                     ','Lamiaceae                     ');
+	 ('Allium','Alliaceae'),
+	 ('Beta','Chenopodiaceae'),
+	 ('Brassica','Brassicaceae'),
+	 ('Capsicum','Solanaceae'),
+	 ('Convallaria','Convallariaceae'),
+	 ('Cucumis','Cucurbitaceae'),
+	 ('Daucus','Apiaceae'),
+	 ('Fragaria','Rosaceae'),
+	 ('Lactuca','Asteraceae'),
+	 ('Lavendula','Lamiaceae');
 INSERT INTO "GardenManager".plantfamily (genus,plantfamily) VALUES
-	 ('Mentha                        ','Lamiaceae                     '),
-	 ('Metha                         ','Lamiaceae                     '),
-	 ('Phaseolus                     ','Leguminosae                   '),
-	 ('Pisum                         ','Fabaceae                      '),
-	 ('Rheum                         ','Polygonaceae                  '),
-	 ('Solanum                       ','Solanaceae                    '),
-	 ('Spinacia                      ','Chenopodiaceae                '),
-	 ('Zea                           ','Gramineae                     ');
+	 ('Mentha','Lamiaceae'),
+	 ('Metha','Lamiaceae'),
+	 ('Phaseolus','Leguminosae'),
+	 ('Pisum','Fabaceae'),
+	 ('Rheum','Polygonaceae'),
+	 ('Solanum','Solanaceae'),
+	 ('Spinacia','Chenopodiaceae'),
+	 ('Zea','Gramineae');

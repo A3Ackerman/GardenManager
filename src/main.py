526bba777bb4b4ddbf1692e7ac77b5d1a848f3d9
derekcswong <derek4b4@gmail.com>
2020-11-11 19:46:35 -0800

    GardenManager connects with postgres db and performs Select statement

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-from sqlalchemy.sql import select
-from sqlalchemy import create_engine, Table
+import psycopg2
 
 user = 'GardenManager'
 pw = 'gardenManager'
@@ -8,8 +7,6 @@ url = '0.0.0.0'
 db = 'GardenManager'
 
 app = Flask(__name__, template_folder="")
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{pw}@{url}/{db}'
-db = create_engine(f'postgresql://{user}:{pw}@{url}/{db}')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,22 +14,23 @@ def hello():
     display = False
     if request.method == 'POST':
         display = True
-
-        """
-        NOT WORKING yet... 
-        with db.connect() as conn:
-            plant = Table('plant', {'mysql_engine':'PostgreSQL'})
-            query = select([plant])
-        for row in conn.execute(query):
-            print(row)
-        """
-
+        connectAndSelect()
     return render_template('GardenManager.html', query1=display)
 
 
 @app.route('/button1')
 def button1():
     return render_template('GardenManager.html', query1=True)
+
+
+def connectAndSelect():
+    conn = psycopg2.connect(f'dbname={db} user={user} password={pw} host={url}')
+    cur = conn.cursor()
+    cur.execute("Select plantID from plant;")
+    print(cur.fetchall())
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 if __name__ == '__main__':

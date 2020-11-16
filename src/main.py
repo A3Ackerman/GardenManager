2e11b2ec6fd0cb974501cdfcc389fef1a302519a
derekcswong <derek4b4@gmail.com>
2020-11-16 10:01:41 -0800

    refactored some code and trying to get update working

--- src/main.py
+++ src/main.py
@@ -26,22 +26,6 @@ def sampleQuery():
     return render_template('GardenManager.html', arrays=arrays)
 
 
-@app.route('/deletePlant', methods=['GET', 'POST'])
-def deletePlant():
-    if request.method == 'POST':
-        plantIDtoDelete = request.form["plantID"]
-        print(f'DELETE FROM plant WHERE plantid={plantIDtoDelete};')
-        deletePlantQ = f'DELETE FROM plant WHERE plantid={plantIDtoDelete};'
-        connectAndQuery(deletePlantQ, False)
-        return render_template('GardenManager.html', arrays=arrays)
-    else:
-        selectPlants = 'Select * from plant order by plantid;'
-        arrays['plant'] = {}
-        arrays['plant']['res'] = connectAndQuery(selectPlants)
-        arrays['plant']['cols'] = ['Plant ID', 'Variety', 'Genus', 'Species', 'Colour', 'Health Status', 'Environment ID']
-        return render_template('GardenManager.html', arrays=arrays)
-
-
 @app.route('/pest', methods=['GET', 'POST'])
 def insertPest():
     if request.method == 'POST':
@@ -67,6 +51,47 @@ def project():
     arrays['project'] = {}
     arrays['project']['res'] = connectAndQuery(projectQ)
     arrays['project']['cols'] = ['variety', 'genus', 'species', 'environmentID']
+    return render_template('GardenManager.html', arrays=arrays)
+
+@app.route('/deletePlant', methods=['GET', 'POST'])
+def deletePlant():
+    if request.method == 'POST':
+        plantIDtoDelete = request.form["plantID"]
+        deletePlantQ = f'DELETE FROM plant WHERE plantid={plantIDtoDelete};'
+        connectAndQuery(deletePlantQ, False)
+        return render_template('GardenManager.html', arrays=arrays)
+    else:
+        return showPlantTable()
+
+
+@app.route('/plant', methods=['GET', 'POST'])
+def plant():
+    #we run into some issues here with deleting from the plant table.
+    # also just found out that forms only work with get and post requests
+    if request.method == 'POST':
+        plantIDtoDelete = request.form["plantID"]
+        deletePlantQ = f'DELETE FROM plant WHERE plantid={plantIDtoDelete};'
+        connectAndQuery(deletePlantQ, False)
+        return render_template('GardenManager.html', arrays=arrays)
+    elif request.method == 'PUT':
+        print("got here")
+        plantID = request.args.get("plantID")
+        healthStatus = request.form["healthStatus"]
+        print(plantID, healthStatus)
+        updatePlantQ = "UPDATE Plant " \
+                       "SET Variety = "", Genus = "", Species = "",Colour = "", HealthStatus = "", EnvironmentID = 0 " \
+                       "WHERE PlantID = 0;"
+        #connectAndQuery(updatePlantQ, False)
+        return render_template('GardenManager.html', arrays=arrays)
+    else:
+        return showPlantTable()
+
+
+def showPlantTable():
+    selectPlants = 'Select * from plant order by plantid;'
+    arrays['plant'] = {}
+    arrays['plant']['res'] = connectAndQuery(selectPlants)
+    arrays['plant']['cols'] = ['Plant ID', 'Variety', 'Genus', 'Species', 'Colour', 'Health Status', 'Environment ID']
     return render_template('GardenManager.html', arrays=arrays)
 
 

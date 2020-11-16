0da02fc17c7e240058b487d5b9e06abc768864c8
jgidds <50605198+jgidds@users.noreply.github.com>
2020-11-15 22:09:23 -0700

    Add DeletePlant
    add deletePlant function

--- src/main.py
+++ src/main.py
@@ -27,13 +27,16 @@ def sampleQuery():
 @app.route('/deletePlant', methods=['GET', 'POST'])
 def deletePlant():
     if request.method == 'POST':
+        plantIDtoDelete = request.args.get("plantID")
+        deletePlantQ = 'DELETE FROM plant WHERE plantid='+ plantIDtoDelete +';'
+        arrays['delPlant'] = {}
+        arrays['delPlant']['plantid'] = connectAndQuery(selectQ)
         return render_template('GardenManager.html', arrays=arrays)
     else:
-        plantIDtoDelete = request.args.get("plantID")
-        selectQ = 'DELETE FROM plant WHERE plantid='+ plantIDtoDelete +';'
-        arrays['sampleQ'] = {}
-        arrays['sampleQ']['res'] = connectAndQuery(selectQ)
-        arrays['sampleQ']['cols'] = ['plantid', 'variety', 'genus', 'species', 'colour', 'healthstatus', 'environmentid']
+        selectPlants = 'Select * from plant order by plantid;'
+        arrays['plant'] = {}
+        arrays['plant']['res'] = connectAndQuery(selectPlants)
+        arrays['plant']['cols'] = ['Plant ID', 'Variety', 'Genus', 'Species', 'Colour', 'Health Status', 'Environment ID']
         return render_template('GardenManager.html', arrays=arrays)
 
 
